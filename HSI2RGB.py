9cea92cea21cdd7315c00b806928ad920b42fb51
dewinn56 <Jens.DeWinne@imec.be>
2024-04-12 18:27:42 +0200

    ++

--- HSI2RGB.py
+++ HSI2RGB.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import scipy.io as spio
 from scipy.interpolate import PchipInterpolator
 from bisect import bisect
@@ -31,7 +32,10 @@ def HSI2RGB(wY,HSI,ydim,xdim,d,threshold):
 
     
     # Load reference illuminant
-    D = spio.loadmat('./D_illuminants.mat')
+    current_file_path = os.path.abspath(__file__)
+    current_dir = os.path.dirname(current_file_path)
+    mat_file_path = os.path.join(current_dir, 'D_illuminants.mat')
+    D = spio.loadmat(mat_file_path)
     w = D['wxyz'][:,0]
     x = D['wxyz'][:,1]
     y = D['wxyz'][:,2]
@@ -42,8 +46,8 @@ def HSI2RGB(wY,HSI,ydim,xdim,d,threshold):
          55:3,
          65:1,
          75:4}
-    wI = D[:,0];
-    I = D[:,i[d]];
+    wI = D[:,0]
+    I = D[:,i[d]]
         
     # Interpolate to image wavelengths
     I = PchipInterpolator(wI,I,extrapolate=True)(wY) # interp1(wI,I,wY,'pchip','extrap')';
@@ -73,40 +77,40 @@ def HSI2RGB(wY,HSI,ydim,xdim,d,threshold):
     # Convert to RGB
     M = np.array([[3.2404542, -1.5371385, -0.4985314],
                   [-0.9692660, 1.8760108, 0.0415560],
-                  [0.0556434, -0.2040259, 1.0572252]]);
-    sRGB=M@XYZ;
+                  [0.0556434, -0.2040259, 1.0572252]])
+    sRGB=M@XYZ
     
     # Gamma correction
-    gamma_map = sRGB >  0.0031308;
-    sRGB[gamma_map] = 1.055 * np.power(sRGB[gamma_map], (1. / 2.4)) - 0.055;
-    sRGB[np.invert(gamma_map)] = 12.92 * sRGB[np.invert(gamma_map)];
+    gamma_map = sRGB >  0.0031308
+    sRGB[gamma_map] = 1.055 * np.power(sRGB[gamma_map], (1. / 2.4)) - 0.055
+    sRGB[np.invert(gamma_map)] = 12.92 * sRGB[np.invert(gamma_map)]
     # Note: RL, GL or BL values less than 0 or greater than 1 are clipped to 0 and 1.
-    sRGB[sRGB > 1] = 1;
-    sRGB[sRGB < 0] = 0;
+    sRGB[sRGB > 1] = 1
+    sRGB[sRGB < 0] = 0
     
     if threshold:
         for idx in range(3):
-            y = sRGB[idx,:];
+            y = sRGB[idx,:]
             a,b = np.histogram(y,100)
             b = b[:-1] + np.diff(b)/2
             a=np.cumsum(a)/np.sum(a)
             th = b[0]
-            i = a<threshold;
+            i = a<threshold
             if i.any():
-                th=b[i][-1];
+                th=b[i][-1]
             y=y-th
             y[y<0] = 0
 
             a,b=np.histogram(y,100)
             b = b[:-1] + np.diff(b)/2
-            a=np.cumsum(a)/np.sum(a);
+            a=np.cumsum(a)/np.sum(a)
             i = a > 1-threshold
             th=b[i][0]
             y[y>th]=th
             y=y/th
             sRGB[idx,:]=y
         
-    R = np.reshape(sRGB[0,:],[ydim,xdim]);
-    G = np.reshape(sRGB[1,:],[ydim,xdim]);
-    B = np.reshape(sRGB[2,:],[ydim,xdim]);
+    R = np.reshape(sRGB[0,:],[ydim,xdim])
+    G = np.reshape(sRGB[1,:],[ydim,xdim])
+    B = np.reshape(sRGB[2,:],[ydim,xdim])
     return np.transpose(np.array([R,G,B]),[1,2,0])
